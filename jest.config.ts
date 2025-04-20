a46603867d8dc17444bc43e93dd1e3d3d8468eb8
tomoya-n <s266227@wakayama-u.ac.jp>
2025-04-21 00:41:57 +0900

    chore: Jestテスト環境の導入
    - テスト実行のためのtsconfigの追加
    - jestの設定ファイルの作成
    - pre-commitにnpm run testを追加

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json",
+      },
+    ],
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+};
