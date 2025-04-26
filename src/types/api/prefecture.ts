08e26395daefcca096387e8087fc9779b34b0dd3
tomoya-n <s266227@wakayama-u.ac.jp>
2025-04-26 16:51:58 +0900

    refactor: 不要な型を削除
    - apiレスポンスの型がaxiosの型を使用して，不要になったため，削除

--- src/types/api/prefecture.ts
+++ src/types/api/prefecture.ts
@@ -3,9 +3,3 @@ export interface Prefecture {
   prefCode: number;
   prefName: string;
 }
-
-// 都道府県一覧APIのレスポンス
-export interface PrefecturesResponse {
-  message: null | string;
-  result: Prefecture[];
-}
