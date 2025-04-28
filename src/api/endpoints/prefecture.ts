6cd8598333fadcb3b3896a9f5b14dd771abb071b
tomoya0318 <134858551+tomoya0318@users.noreply.github.com>
2025-04-28 19:26:51 +0900

    都道府県一覧を取得するapiとUIを接続 (#19)
    * fix: .envファイルからの読み込みができていなかったため，修正
    
    * fix: 帰ってくるデータの形式が違ったため，修正
    
    * feat: apiとuiの接続
    
    - ローディング画面を追加
    - エラー時の画面を追加
    - データがない場合の対応

--- src/api/endpoints/prefecture.ts
+++ src/api/endpoints/prefecture.ts
@@ -1,8 +1,13 @@
 import { Prefecture } from "../../types/domain/prefecture";
 import { fetcher } from "../fetcher";
 
+interface PrefectureResponse {
+  message: string;
+  result: Prefecture[];
+}
+
 // 都道府県一覧を取得するAPI
 export const getPrefectures = async (): Promise<Prefecture[]> => {
-  const response = await fetcher.get<Prefecture[]>("/prefectures");
-  return response.data;
+  const response = await fetcher.get<PrefectureResponse>("/prefectures");
+  return response.data.result;
 };
