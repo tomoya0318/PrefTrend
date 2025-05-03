c12189bf6ea07e5f1ecaafd0e1460d7de2477955
tomoya-n <s266227@wakayama-u.ac.jp>
2025-05-03 20:49:18 +0900

    refactor: 関心の分離
    - providerなどのインフラ設定をmain.tsxに移行し，app.tsxにはアプリケーションロジックのみにする

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-
 import { Dashboard } from "./components/pages/Dashboard";
-import { queryClient } from "./utils/queryClient";
 
 export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Dashboard />
-    </QueryClientProvider>
-  );
+  return <Dashboard />;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App.tsx";
+import { queryClient } from "./utils/queryClient.ts";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
